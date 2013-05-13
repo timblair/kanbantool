2187faa9914adaff06b83c6f65207fc8ac438507
Tim Blair <tim@bla.ir>
2013-05-13 16:46:55 +0100

    Add client specs for retrieving tasks

--- spec/lib/kanbantool/client_spec.rb
+++ spec/lib/kanbantool/client_spec.rb
@@ -13,12 +13,30 @@ describe KanbanTool::Client do
       VCR.insert_cassette "boards"
     end
 
+    subject { client.boards }
+
     it "returns an array" do
-      client.boards.must_be_instance_of(Array)
+      subject.must_be_instance_of(Array)
     end
 
     it "returns an array of boards" do
-      client.boards.each { |b| b.must_be_instance_of(KanbanTool::Board) }
+      subject.each { |b| b.must_be_instance_of(KanbanTool::Board) }
+    end
+  end
+
+  describe "#tasks" do
+    before do
+      VCR.insert_cassette "tasks"
+    end
+
+    subject { client.tasks(12345) }
+
+    it "returns an array" do
+      subject.must_be_instance_of(Array)
+    end
+
+    it "returns an array of boards" do
+      subject.each { |b| b.must_be_instance_of(KanbanTool::Task) }
     end
   end
 
